3c6c93600d47c9493895b7855a007ae2495c5b6d
Lucas David <lucasdavidoliveira80@gmail.com>
2026-04-10 18:16:30 -0300

    Test: add testes negativos para números

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 temp_conv = TemperatureConverter()
 
+
 @app.route("/")
 def home():
     return jsonify({
@@ -19,19 +20,22 @@ def home():
         ]
     })
 
+
 @app.route("/numeros")
 def rota_numeros():
     n = request.args.get("n", 10, type=int)
     return jsonify({
-        "original": n,
+        "decimal": n,
         "binario": decimal_a_binario(n),
         "octal": decimal_a_octal(n),
         "hexadecimal": decimal_a_hexadecimal(n)
     })
 
+
 @app.route("/moedas")
 def rota_moedas():
     return jsonify({"suportadas": listar_moedas()})
+
 
 @app.route("/converter")
 def rota_converter():
@@ -46,23 +50,25 @@ def rota_converter():
     except Exception as e:
         return jsonify({"erro": str(e)}), 400
 
+
 @app.route("/temperatura")
 def rota_temperatura():
     try:
         v = request.args.get("valor", 0.0, type=float)
         u = request.args.get("unidade", "C").upper()
-        
+
         if u == "C":
-            res = {"K": temp_conv.celsius_to_kelvin(v), "F": temp_conv.celsius_to_fahrenheit(v)}
+            res = {"K": temp_conv.celsius_to_kelvin(
+                v), "F": temp_conv.celsius_to_fahrenheit(v)}
         elif u == "F":
             res = {"C": temp_conv.fahrenheit_to_celsius(v)}
         elif u == "K":
             res = {"C": temp_conv.kelvin_to_celsius(v)}
         else:
             return jsonify({"erro": "Use unidade C, F ou K"}), 400
-            
+
         return jsonify({"entrada": v, "unidade": u, "conversoes": res})
     except Exception as e:
         return jsonify({"erro": str(e)}), 400
-    
-    #teste
+
+    # teste
